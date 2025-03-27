4efd2eed166fe506667f3f7b7549dc8ebc849036
lminjiii <lminjiiiii@gmail.com>
2025-03-27 11:22:03 +0900

    cors update

--- src/main/java/com/my/backend/BackendApplication.java
+++ src/main/java/com/my/backend/BackendApplication.java
@@ -1,13 +1,21 @@
 package com.my.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
